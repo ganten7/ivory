4628ed5d0c12d573ecc28f4dd929f9c78e65015c
ganten7 <ganten7@users.noreply.github.com>
2025-12-14 00:41:56 -0600

    Add PyInstaller hook to exclude QtBluetooth framework and handle symlink conflicts

---
+++ hook-PyQt5.QtBluetooth.py
@@ -0,0 +1,11 @@
+# PyInstaller hook to exclude QtBluetooth framework
+from PyInstaller.utils.hooks import collect_submodules, collect_data_files, collect_dynamic_libs
+
+# Exclude QtBluetooth completely
+excludedimports = ['PyQt5.QtBluetooth']
+
+# Don't collect any binaries for QtBluetooth
+binaries = []
+
+# Don't collect any data files
+datas = []
